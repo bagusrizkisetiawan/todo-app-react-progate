f8c3db23f7b6a277275004b22aaef1fcde4008f7
Bagus Rizki Setiawan <bagus30122002@gmail.com>
2024-05-25 10:32:50 +0700

    Develop toggle todo feature

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,11 +1,25 @@
 import React from "react";
 
-const TodoItem = ({ todo }) => {
+const TodoItem = ({ todo, toggleCompleted }) => {
+  function todoListStyle() {
+    if (todo.completed) {
+      return "text-success mb-0 fw-bold";
+    } else {
+      return "mb-0";
+    }
+  }
+
   return (
     <>
       <div className="card mb-2">
-        <div className="card-body">
-          <p className="mb-0">{todo.title}</p>
+        <div className="card-body d-flex gap-2">
+          <input
+            type="checkbox"
+            name=""
+            id=""
+            onChange={() => toggleCompleted(todo.id)}
+          />
+          <p className={todoListStyle()}>{todo.title}</p>
         </div>
       </div>
     </>
